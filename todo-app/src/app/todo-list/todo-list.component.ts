2db9eb102bb17bba1ca4754a02b630d323a068c3
AlexMotyka <motykaalex@gmail.com>
2020-01-19 11:31:00 -0500

    Add ngmodule for add todo

--- todo-app/src/app/todo-list/todo-list.component.ts
+++ todo-app/src/app/todo-list/todo-list.component.ts
@@ -1,15 +1,28 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/todo.service';
 
 @Component({
   selector: 'todo-list',
   templateUrl: './todo-list.component.html',
-  styleUrls: ['./todo-list.component.scss']
+  styleUrls: ['./todo-list.component.scss'],
+  providers: [TodoService],
 })
 export class TodoListComponent implements OnInit {
 
-  constructor() { }
+  // stores the todo name the user types
+  todoName: string;
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.todoName = '';
+  }
+
+  addTodo(): void {
+    if (this.todoName.trim().length === 0) {
+      this.todoName = 'YOU FORGOT TO NAME ME';
+    }
+    this.todoService.addTodo(this.todoName);
+    this.todoName = '';
   }
 
 }
