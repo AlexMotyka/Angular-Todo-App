8b3220c930c98009580fc16acc6976a05f2d241a
AlexMotyka <motykaalex@gmail.com>
2020-01-19 08:48:22 -0500

    Added addTodo

--- todo-app/src/app/todo.service.ts
+++ todo-app/src/app/todo.service.ts
@@ -1,9 +1,43 @@
 import { Injectable } from '@angular/core';
+import { Todo } from 'todo';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TodoService {
 
+  nextId: number = 3
+  todoName: string = '';
+  // hard coded todos for now
+  todos: Todo[] = [
+    {
+      'id': 1,
+      'name': 'Finish assignment 1',
+      'isEditing': false,
+      'isCompleted': false,
+    },
+    {
+      'id': 2,
+      'name': 'Finish Capstone',
+      'isEditing': false,
+      'isCompleted': false,
+    },
+  ];
   constructor() { }
+
+  addTodo(todoName: string): void {
+    // Make sure the name is not empty
+    if(todoName.trim().length === 0){
+      todoName = "YOU FORGOT TO NAME ME";
+    }
+
+    this.todos.push({
+      id: this.nextId,
+      name: todoName,
+      isEditing: false,
+      isCompleted: false
+    });
+
+    this.nextId = this.nextId + 1;
+  }
 }
