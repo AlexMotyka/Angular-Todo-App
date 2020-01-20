32ab3ff63d51675ad08ed05eba5cce3ba5d219b8
AlexMotyka <motykaalex@gmail.com>
2020-01-20 13:54:35 -0500

    Initial server commit

---
+++ node-server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const mysql = require('mysql');
+const app = express();
+const port = 3000
+
+app.get('/', (req, res) => res.send('Hello World!'))
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+
+var connection = mysql.createConnection({
+  host     : 'localhost',
+  user     : 'root',
+  password : 'root',
+  database : 'tododb'
+});
+
+connection.connect();
+
+connection.query('SELECT * FROM Todos;', function (error, results, fields) {
+  if (error) throw error;
+  console.log('The solution is: ', results);
+});
+
+connection.end();
