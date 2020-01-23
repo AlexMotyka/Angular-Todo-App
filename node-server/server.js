e9edf82864f218a5c5895012f92ec3e514799c24
AlexMotyka <motykaalex@gmail.com>
2020-01-22 19:31:24 -0500

    Add some comments

--- node-server/server.js
+++ node-server/server.js
@@ -5,13 +5,19 @@ const Promise = require('promise');
 const bodyParser = require('body-parser')
 const app = express();
 const port = 3000
+
 // parse application/json
 app.use(bodyParser.json())
+// this sets response headers to allow cross origin requests
 app.use(cors())
 
+// API routes
+
+// get the desired todos given the filter (All, active, or completed)
 app.get('/todos', function (req, res) {
   var filter = '';
   filter = req.query.filter;
+  // retrieve the todos and send them as a response
   getTodos(filter)
   .then(function(todos, error) {
         if(error) throw error;
@@ -19,22 +25,25 @@ app.get('/todos', function (req, res) {
     });
 })
 
+// create a todo
 app.post('/todo', function (req, res) {
   var name = req.body.name;
   addTodo(name)
   .then(function(insertId, error) {
         if(error) throw error;
-        console.log("Insert ID before sending: ", insertId);
+        // send back the id of the created todo
         res.send(insertId.toString());
     });
 })
 
+// delete a todo given its id
 app.delete('/todo/:id', function (req, res) {
   var id = req.params.id;
   deleteTodo(id)
   res.send('200')
 })
 
+// update a todo's name, completion status given its id
 app.put('/todo', function (req, res) {
   var id = req.body.id;
   var name = req.body.name;
@@ -49,8 +58,10 @@ app.put('/todo', function (req, res) {
   res.send('200')
 })
 
+// start listening for requests
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
 
+// make a connection with the database
 const connection = mysql.createConnection({
   host     : 'localhost',
   user     : 'root',
@@ -60,9 +71,10 @@ const connection = mysql.createConnection({
 
 connection.connect();
 
+// get todos based on the filter
 function getTodos(filter){
   return new Promise(function(resolve, reject) {
-    // default
+    // default is all todos
     var condition = "'completed' = 0 OR 'completed' = 1;"
 
     if(filter === 'Completed'){
@@ -79,6 +91,7 @@ function getTodos(filter){
   });
 }
 
+// add a new todo given a name
 function addTodo(name){
 
   return new Promise(function(resolve, reject) {
@@ -89,6 +102,7 @@ function addTodo(name){
   });
 }
 
+// delete a todo given the id
 function deleteTodo(id){
   connection.query(`DELETE FROM Todos WHERE id = '${id}';`, function (error, results, fields) {
     if (error) throw error;
@@ -96,6 +110,7 @@ function deleteTodo(id){
   });
 }
 
+// update a todo
 function updateTodo(id, name, completed){
   connection.query(`UPDATE Todos SET name = '${name}', completed = '${completed}' WHERE id = '${id}';`, function (error, results, fields) {
     if (error) throw error;
