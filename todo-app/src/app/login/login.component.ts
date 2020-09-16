07c04cbe7b9dc38178df4778984c032aca7c6675
Alex Motyka <motykaalex@gmail.com>
2020-09-16 12:46:06 -0400

    Add login skeleton

--- todo-app/src/app/login/login.component.ts
+++ todo-app/src/app/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { TodoService } from 'src/app/todo.service';
 
 @Component({
   selector: 'login',
@@ -7,9 +8,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  email: string;
+  password: string;
+  token: string
+
+  constructor(private todoService: TodoService) { }
 
   ngOnInit() {
+    this.email = ""
+    this.password = ""
+  }
+
+  login(): void {
+    console.log("login!")
+    if (this.email.trim().length === 0){
+      console.log("you forgot to enter an email")
+    } else {
+      // go to todos page
+    }
+  }
+
+  signup(): void {
+    console.log("sign up!")
   }
 
 }
