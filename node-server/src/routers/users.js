3f866aae485094ce034592c6be5a366625a250eb
Alex Motyka <motykaalex@gmail.com>
2020-09-16 12:01:57 -0400

    add password hashing

--- node-server/src/routers/users.js
+++ node-server/src/routers/users.js
@@ -40,6 +40,14 @@ router.post('/user', async (req, res) => {
     }
 })
 
+router.post('/user/login', async (req, res) => {
+    try {
+        const user = await User.findByCredentials(req.body.email, req.body.password)
+        res.send(user)
+    } catch (error) {
+        res.status(400).send()
+    }
+})
 router.put('/user/:id', async (req, res) => {
     const updates = Object.keys(req.body)
     const allowedUpdates = ['name', 'email', 'password', 'age']
